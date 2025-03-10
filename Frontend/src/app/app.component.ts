d735fd0b90056dcae3796f38c71a846a30e05626
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-10 21:12:09 +0100

    feat(config): provide HttpClient

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './features/menu/menu.component';
 import { CombatSceneComponent } from './features/combat-scene/combat-scene.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CombatSceneComponent],
+  imports: [MenuComponent, CombatSceneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Frontend';
-}
+export class AppComponent {}
