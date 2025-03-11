f495ffb448488c90b895adbd079c68b77d2ada0f
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-11 23:47:18 +0100

    feat(component): add team list of selected characters (unfinished)

---
+++ Frontend/src/app/features/menu/team-list/team-list.component.ts
@@ -0,0 +1,35 @@
+import { NgFor } from '@angular/common';
+import { Component, inject, Input, input, OnInit, signal, Signal } from '@angular/core';
+import { GameService } from '../../../services/game.service';
+import { MenuService } from '../../../services/menu.service';
+import { CharacterTeam } from '../../../models/character-team.model';
+import { CharacterDTO } from '../../../models/character-dto.model';
+import { CharacterTileComponent } from "../character-tile/character-tile.component";
+
+@Component({
+  selector: 'app-team-list',
+  imports: [NgFor, CharacterTileComponent],
+  templateUrl: './team-list.component.html',
+  styleUrl: './team-list.component.css'
+})
+export class TeamListComponent implements OnInit {
+  @Input({ required: true }) teamSide!: CharacterTeam;
+
+  private readonly gameService = inject(GameService);
+  private readonly menuService = inject(MenuService);
+
+  defaultCharacters = this.gameService.defaultCharacters;
+  selectedCharacters : Signal<CharacterDTO[]> = signal([]);
+
+  addCharacter(character: CharacterDTO){
+    this.menuService.addCharacter(character, this.teamSide);
+  }
+
+  removeCharacter(index : number){
+    this.menuService.removeCharacter(index, this.teamSide);
+  }
+
+  ngOnInit(): void {
+    this.selectedCharacters = this.menuService.getSelectedCharacters(this.teamSide);
+  }
+}
