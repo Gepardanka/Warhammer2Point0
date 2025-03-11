3a6b60f23bb8011b8446f319c4bac9c7f9b5388d
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-11 23:48:18 +0100

    feat(component): implement team lists to menu

--- Frontend/src/app/features/menu/menu.component.ts
+++ Frontend/src/app/features/menu/menu.component.ts
@@ -1,17 +1,26 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import { NgFor } from '@angular/common';
+import { CharacterDTO } from '../../models/character-dto.model';
+import { TeamListComponent } from "./team-list/team-list.component";
+import { CharacterTeam } from '../../models/character-team.model';
 
 @Component({
   selector: 'app-menu',
-  imports: [NgFor],
+  imports: [TeamListComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
 export class MenuComponent implements OnInit {
-  gameService = inject(GameService);
+  private readonly gameService = inject(GameService);
 
-  defaultCharacters = this.gameService.defaultCharacters;
+  CharacterTeam = CharacterTeam;
+  isGameLoading = false;
+
+  startGame(){
+    //this.gameService.fetchBattleHistory();
+    this.isGameLoading = true;
+  }
 
   ngOnInit(): void {
     this.gameService.fetchDefaultCharacters();
