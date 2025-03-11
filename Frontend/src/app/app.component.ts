479d089db721f8cafc5374a9cdfe35671880b364
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-11 23:44:38 +0100

    feat(app): add switching game views in app component

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MenuComponent } from './features/menu/menu.component';
 import { CombatSceneComponent } from './features/combat-scene/combat-scene.component';
+import { NgIf } from '@angular/common';
+import { GameService } from './services/game.service';
 
 @Component({
   selector: 'app-root',
-  imports: [MenuComponent, CombatSceneComponent],
+  imports: [MenuComponent, CombatSceneComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  gameService = inject(GameService);
+  showGame = this.gameService.gameStarted;
+}
