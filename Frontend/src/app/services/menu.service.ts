59e3997981663698ad46c8d2ecf34886b41ddd5b
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-11 23:51:52 +0100

    feat(MenuService): add service for managing team lists in the menu

---
+++ Frontend/src/app/services/menu.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { CharacterDTO } from '../models/character-dto.model';
+import { CharacterTeam } from '../models/character-team.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MenuService {
+  private selectedCharactersLeft = signal<CharacterDTO[]>([]);
+  private selectedCharactersRight = signal<CharacterDTO[]>([]);
+
+  getSelectedCharacters(team: CharacterTeam) {
+    switch (team) {
+      case CharacterTeam.TeamA:
+        console.log('Team A');
+        return this.selectedCharactersLeft.asReadonly();
+      case CharacterTeam.TeamB:
+        console.log('Team B');
+        return this.selectedCharactersRight.asReadonly();
+      default:
+        console.error('Wrong team selected');
+        return this.selectedCharactersLeft.asReadonly();
+    }
+  }
+
+  addCharacter(char: CharacterDTO, team: CharacterTeam) {
+    switch (team) {
+      case CharacterTeam.TeamA:
+        console.log('Team A');
+        this.addCharacterToCollection(this.selectedCharactersLeft, char);
+        break;
+      case CharacterTeam.TeamB:
+        console.log('Team B');
+        this.addCharacterToCollection(this.selectedCharactersRight, char);
+        break;
+    }
+  }
+
+  removeCharacter(index: number, team: CharacterTeam) {
+    switch (team) {
+      case CharacterTeam.TeamA:
+        this.removeCharacterToCollection(this.selectedCharactersLeft, index);
+        break;
+      case CharacterTeam.TeamB:
+        this.removeCharacterToCollection(this.selectedCharactersRight, index);
+        break;
+    }
+  }
+
+  private addCharacterToCollection(
+    collection: WritableSignal<CharacterDTO[]>,
+    char: CharacterDTO
+  ) {
+    collection.update((array) => [...array, char]);
+  }
+
+  private removeCharacterToCollection(
+    collection: WritableSignal<CharacterDTO[]>,
+    index: number
+  ) {
+    collection.update((array) => {
+      const newArray = [...array];
+      newArray.splice(index, 1);
+      return newArray;
+    });
+  }
+}
