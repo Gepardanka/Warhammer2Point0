7711ba19fe61ba6705a6c0934377a5a345c5dadc
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-16 13:40:13 +0100

    fix(team-list): pass copy of character object instead of original reference

--- Frontend/src/app/features/menu/team-list/team-list.component.ts
+++ Frontend/src/app/features/menu/team-list/team-list.component.ts
@@ -1,16 +1,23 @@
 import { NgFor } from '@angular/common';
-import { Component, inject, Input, input, OnInit, signal, Signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnInit,
+  signal,
+  Signal,
+} from '@angular/core';
 import { GameService } from '../../../services/game.service';
 import { MenuService } from '../../../services/menu.service';
 import { CharacterTeam } from '../../../models/character-team.model';
 import { CharacterDTO } from '../../../models/character-dto.model';
-import { CharacterTileComponent } from "../character-tile/character-tile.component";
+import { CharacterTileComponent } from '../character-tile/character-tile.component';
 
 @Component({
   selector: 'app-team-list',
   imports: [NgFor, CharacterTileComponent],
   templateUrl: './team-list.component.html',
-  styleUrl: './team-list.component.css'
+  styleUrl: './team-list.component.css',
 })
 export class TeamListComponent implements OnInit {
   @Input({ required: true }) teamSide!: CharacterTeam;
@@ -19,17 +26,21 @@ export class TeamListComponent implements OnInit {
   private readonly menuService = inject(MenuService);
 
   defaultCharacters = this.gameService.defaultCharacters;
-  selectedCharacters : Signal<CharacterDTO[]> = signal([]);
+  selectedCharacters: Signal<CharacterDTO[]> = signal([]);
 
-  addCharacter(character: CharacterDTO){
-    this.menuService.addCharacter(character, this.teamSide);
+  addCharacter(character: CharacterDTO) {
+    const characterCopy = { ...character };
+    characterCopy.team = this.teamSide;
+    this.menuService.addCharacter(characterCopy);
   }
 
-  removeCharacter(index : number){
+  removeCharacter(index: number) {
     this.menuService.removeCharacter(index, this.teamSide);
   }
 
   ngOnInit(): void {
-    this.selectedCharacters = this.menuService.getSelectedCharacters(this.teamSide);
+    this.selectedCharacters = this.menuService.getSelectedCharacters(
+      this.teamSide
+    );
   }
 }
