d2b634a03d3e13a4df6cf49356606d81f948ef51
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-16 10:58:20 +0100

    feat(menu): add startBattle function with loading screen

--- Frontend/src/app/features/menu/menu.component.ts
+++ Frontend/src/app/features/menu/menu.component.ts
@@ -1,25 +1,39 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { GameService } from '../../services/game.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { CharacterDTO } from '../../models/character-dto.model';
-import { TeamListComponent } from "./team-list/team-list.component";
+import { TeamListComponent } from './team-list/team-list.component';
 import { CharacterTeam } from '../../models/character-team.model';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-menu',
-  imports: [TeamListComponent],
+  imports: [TeamListComponent, NgIf],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
 export class MenuComponent implements OnInit {
   private readonly gameService = inject(GameService);
+  private readonly menuService = inject(MenuService);
+
+  private selectedCharacters = this.menuService.getSelectedCharacters(
+    CharacterTeam.Both
+  );
 
   CharacterTeam = CharacterTeam;
-  isGameLoading = false;
+  isGameLoading = signal(false);
 
-  startGame(){
-    //this.gameService.fetchBattleHistory();
-    this.isGameLoading = true;
+  constructor() {
+    effect(() => {
+      if (this.gameService.gameStarted()) {
+        this.isGameLoading.set(false);
+      }
+    });
+  }
+
+  startBattle() {
+    this.gameService.fetchBattleHistory(this.selectedCharacters());
+    this.isGameLoading.set(true);
   }
 
   ngOnInit(): void {
